fcdb9517a30b3ff41e8ec0ec17f2a8b099037f73
arya-bhanu <aryacreative.dev@gmail.com>
2024-11-28 05:55:48 +0700

    solved kata

--- src/main/java/org/example/Kata.java
+++ src/main/java/org/example/Kata.java
@@ -1,16 +1,14 @@
 package org.example;
 
 public class Kata {
-    public static boolean solution(String str, String ending) {
-        if(ending.length() > str.length()) return false;
-        int counter = 0;
-        for(int i = ending.length() - 1; i >= 0; i--){
-            char c = str.charAt(str.length() - 1 - counter);
-            if(c != ending.charAt(i)){
-                return false;
+    public static String generateShape(int n) {
+       StringBuilder result = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            result.append("+".repeat(n));
+            if(i < n - 1){
+                result.append("\n");
             }
-            counter++;
         }
-        return true;
+        return result.toString();
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -8,26 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
     @Test
-    public void staticTests() {
-        check("samurai", "ai", true);
-        check("sumo", "omo", false);
-        check("ninja", "ja", true);
-        check("sensei", "i", true);
-        check("samurai", "ra", false);
-        check("abc", "abcd", false);
-        check("abc", "abc", true);
-        check("abcabc", "bc", true);
-        check("fails", "ails", true);
-        check("this", "", true);
-        check(":-)", ":-(", false);
-        check("!@#$%^&*() :-)", ":-)", true);
-        check("abc\n", "abc", false);
-        check("ails", "fails", false);
-        check("this", "fails", false);
-    }
-
-    private static void check(String str, String ending, boolean expected) {
-        boolean result = Kata.solution(str, ending);
-        assertEquals(expected, result, "Expected solution(\"" + str + "\", \"" + ending + "\") to return " + expected);
+    public void exampleTests() {
+        assertEquals("+++\n+++\n+++", Kata.generateShape(3));
+        assertEquals("+++++\n+++++\n+++++\n+++++\n+++++", Kata.generateShape(5));
+        assertEquals("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++", Kata.generateShape(8));
     }
 }
