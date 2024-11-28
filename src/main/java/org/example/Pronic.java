cdf82d7fc28b0c0dde961113fe297da0eaadbb9b
arya-bhanu <aryacreative.dev@gmail.com>
2024-11-29 06:11:42 +0700

    solved kata

--- src/main/java/org/example/Pronic.java
+++ src/main/java/org/example/Pronic.java
@@ -1,11 +1,6 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(isPronic(42));
-        System.out.println("Hello world!");
-    }
-
+public class Pronic {
     public static boolean isPronic(int n) {
         if(n == 0) return true;
         int flored = (int) Math.floor(Math.sqrt(n));
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -3,14 +3,21 @@ package org.example;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class SolutionTest {
     @Test
-    public void exampleTests() {
-        assertEquals("+++\n+++\n+++", Kata.generateShape(3));
-        assertEquals("+++++\n+++++\n+++++\n+++++\n+++++", Kata.generateShape(5));
-        assertEquals("++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++", Kata.generateShape(8));
+    @DisplayName("Test known pronic and non-pronic numbers")
+    void isPronicTest() {
+        assertTrue(Pronic.isPronic(0), "0 is a Pronic Number");
+        assertFalse(Pronic.isPronic(1), "1 is not a Pronic Number");
+        assertTrue(Pronic.isPronic(2), "2 is a Pronic Number");
+        assertFalse(Pronic.isPronic(3), "3 is not a Pronic Number");
+        assertFalse(Pronic.isPronic(4), "4 is not a Pronic Number");
+        assertFalse(Pronic.isPronic(5), "5 is not a Pronic Number");
+        assertTrue(Pronic.isPronic(6), "6 is a Pronic Number");
+        assertFalse(Pronic.isPronic(-3), "-3 is not a Pronic Number");
+        assertFalse(Pronic.isPronic(-27), "-27 is not a Pronic Number");
     }
 }
