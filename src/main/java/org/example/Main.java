eeb207ab03376b8c2782d89b940e354eb3cffcba
arya-bhanu <aryacreative.dev@gmail.com>
2024-12-09 22:17:38 +0700

    solved

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,23 @@
 package org.example;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         long a = 9L;
         long b = 4L;
-        System.out.println(Long.toBinaryString(a));
-        System.out.println(Long.toBinaryString(b));
+        Map<Integer, Integer> map = new HashMap<>();
+        Integer res = map.put(1,100);
+        Integer res2 = map.put(2,200);
+        Integer res3 = map.put(1,1000);
+        System.out.println(res);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(map.get(1));
+     /*   System.out.println(Long.toBinaryString(a));
+        System.out.println(Long.toBinaryString(b));*/
     }
 
 }
