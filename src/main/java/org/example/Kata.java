df2b28a8e09c389c01c6dc07c8912829a812d2fd
arya-bhanu <aryacreative.dev@gmail.com>
2024-12-10 15:25:08 +0700

    solved

--- src/main/java/org/example/Kata.java
+++ src/main/java/org/example/Kata.java
@@ -1,20 +1,12 @@
 package org.example;
 
+import java.util.stream.IntStream;
+
 public class Kata
 {
-    public static boolean interlockable(long a, long b) {
-        String binA = Long.toBinaryString(a);
-        String binB = Long.toBinaryString(b);
-        long lastIndexA = binA.length() - 1;
-        long lastIndexB = binB.length() -  1;
-        while (lastIndexA >= 0 && lastIndexB >= 0){
-            if((binA.charAt((int)lastIndexA) == '1' &&  binB.charAt((int) lastIndexB) == '1')){
-                return false;
-            }
-            --lastIndexB;
-            --lastIndexA;
-        }
 
-        return true;
+    public static int missingNo(int[] nums) {
+        int sumAll = IntStream.of(nums).sum();
+        return (101 * 50) - sumAll;
     }
 }
